566ead126505b74d9a789d7e9b6c7b51550c5e46
Russel-DH <44677975+Russel-DH@users.noreply.github.com>
2024-06-11 19:05:31 +0300

    Update module_2_3.py
    >=     became      >

--- module_2_3.py
+++ module_2_3.py
@@ -4,7 +4,7 @@ i = 0
 while i <= len(my_list):
     if my_list[i] == 0:
         i += 1
-    elif my_list[i] >= 0:
+    elif my_list[i] > 0:
         print(my_list[i])
         i += 1
     else: break
